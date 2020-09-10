bb90fbbeef6b472a5c5a85455b059180e6b481e2
amichalo421 <andre.michalowski@gmail.com>
2020-09-10 16:20:17 -0400

    Task 2: Step 6 - Providing data with CartContext

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //Context
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 
 function App() {
@@ -21,20 +22,20 @@ function App() {
 
 	return (
 		<div className="App">
-
-			<Navigation cart={cart} />
-
 			<ProductContext.Provider value={{ products, addItem }}>
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
+				<CartContext.Provider value={cart}>
+					<Navigation/>
 
-				<Route path="/cart">
-					<ShoppingCart />
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
-
 		</div>
 	);
 }
